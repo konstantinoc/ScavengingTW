b729a5afb7ebd0e8dda6a39f4ed8cdea44b5ee42
konstantinos99 <kotchios99@gmail.com>
2020-05-16 04:25:29 +0300

    Second btn added. It selects and sends all spears for scaveage. All levels returns at the same time.

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ var bor = document.getElementById("scavenge_screen");
 
 var table = document.createElement("table");
 table.setAttribute("class", "candidate-squad-widget vis");
-table.style.width = "200px";
+table.style.width = "100px";
 table.style.marginLeft = "5px";
 
 var tbody = document.createElement("tbody");
@@ -51,24 +51,43 @@ td.appendChild(btn);
 tr.appendChild(td);
 
 tbody.appendChild(tr);
+
+tr = document.createElement("tr");
+td = document.createElement("td");
+var btnSendAll = document.createElement('a');
+btnSendAll.setAttribute("id", "btn_start");
+btnSendAll.setAttribute("class", "btn btn-default");
+//btnSendAll.style.marginLeft = "30px";
+btnSendAll.style.width = "70px";
+btnSendAll.innerHTML = "Send all";
+td.appendChild(btnSendAll);
+tr.appendChild(td);
+
+tbody.appendChild(tr);
 table.appendChild(tbody);
 bor.appendChild(table);
 
-//var spear = document.querySelector('[class = "units-entry-all squad-village-required"][data-unit = "spear"]');
-//spear = spear.innerHTML;
-//spear = spear.slice(1,spear.length-1);
-
-//var current = spear;
+var current;
+btnSendAll.onclick = function(){
+    var spear = document.querySelector('[class = "units-entry-all squad-village-required"][data-unit = "spear"]');
+    spear = spear.innerHTML;
+    spear = spear.slice(1,spear.length-1);
+    current = spear;
+    sendTroops();
+}
 
 btn.onclick= function(){
+    current = parseInt(txtSpear.value,10);
+    sendTroops();
+}
+
+function sendTroops(){
     var btns = document.getElementsByClassName("btn btn-default free_send_button");
-    var current = parseInt(txtSpear.value,10);
     var ration = [1.602, 2.5, 2, 1.5];
     var i;
     for(i=0; i<btns.length; i++){
         current = Math.round(current / ration[i]);
         $("input.unitsInput[name=spear]").val(current).trigger("change");
         btns[i].click();
-
     }
 }
